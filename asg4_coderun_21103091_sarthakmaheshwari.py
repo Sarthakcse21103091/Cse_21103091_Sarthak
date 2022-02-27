7bf85d4405842b9949dde104a2d9eb5f4253d925
Sarthakcse21103091 <99641239+Sarthakcse21103091@users.noreply.github.com>
2022-02-27 18:12:28 +0530

    Add files via upload

---
+++ asg4_coderun_21103091_sarthakmaheshwari.py
@@ -0,0 +1,70 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: D:\ESC1 assignments\asg4_sourcecode_21104099_mehulbansal.py =====
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+Enter the size of pascals triangle 10
+USING LOOPS
+           1 
+          1 1 
+         1 2 1 
+        1 3 3 1 
+       1 4 6 4 1 
+      1 5 10 10 5 1 
+     1 6 15 20 15 6 1 
+    1 7 21 35 35 21 7 1 
+   1 8 28 56 70 56 28 8 1 
+  1 9 36 84 126 126 84 36 9 1 
+
+
+
+USING RECURSSIONS
+                  1   
+                1   1   
+              1   2   1   
+            1   3   3   1   
+          1   4   6   4   1   
+        1   5   10   10   5   1   
+      1   6   15   20   15   6   1   
+    1   7   21   35   35   21   7   1   
+  1   8   28   56   70   56   28   8   1   
+1   9   36   84   126   126   84   36   9   1   
+
+
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+Enter 2 numbers: 10 35
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> The quotient is zero
+<c> Filtered out numbers that are greater than 4 are : [14, 15, 5, 15, 6, 16]
+<d> Set: {5, 6, 14, 15, 16}
+<e> Immutable set: frozenset({16, 5, 6, 14, 15})
+<f> Hash value of the max value from the above set: 16
+
+
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+Object created
+The name of the student it Sarthak Maheshwari and SID is 21103091.
+Object destroyed
+
+
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+Enter the word: sarthak
+Enter a new meaningful word using the exact same letters to test your friendship: arshtka
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
